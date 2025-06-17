a099e6c3f36a9a40f2d44f23571aa58336b9ecbc
Aleksei <4stm4.74rus@gmail.com>
2025-06-17 10:33:33 +0500

    changed logo

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,6 @@
 // | || |_/ __| __| '_ ` _ \| || |_
 // |__   _\__ | |_| | | | | |__   _|
 //   |_| |___/\__|_|_|_| |_|  |_|
-//  ___   ___ _   _| | |_ _   _ _ __ ___
-// / _ \ / __| | | | | __| | | | '_ ` _ \
-//| (_) | (__| |_| | | |_| |_| | | | | | |
-// \___/ \___|\__,_|_|\__|\__,_|_| |_| |_|
 //! # ehatrom — EEPROM HAT library for Raspberry Pi HATs
 //! - [Documentation (docs.rs)](https://docs.rs/ehatrom)
 //! - [GitHub](https://github.com/4stm4/ehatrom)
